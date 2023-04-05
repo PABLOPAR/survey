c1810306997a0f5114c8b729e28e691ace6b1dcf
PABLOPAR <pabparodi@gmail.com>
2023-04-05 12:01:27 +0200

    MySQL Config Pom and Properties

--- src/main/java/com/pablop/survey/web/app/models/entity/Person.java
+++ src/main/java/com/pablop/survey/web/app/models/entity/Person.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-import org.checkerframework.common.aliasing.qual.Unique;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 

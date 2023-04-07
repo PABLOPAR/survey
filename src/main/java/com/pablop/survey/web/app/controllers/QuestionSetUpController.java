fd7e6b033e1f687fdbc3a5ac1645f1dded6f5d34
PABLOPAR <pabparodi@gmail.com>
2023-04-07 22:23:15 +0200

    Category Question Form

---
+++ src/main/java/com/pablop/survey/web/app/controllers/QuestionSetUpController.java
@@ -0,0 +1,81 @@
+package com.pablop.survey.web.app.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pablop.survey.web.app.models.entity.CategoryOption;
+import com.pablop.survey.web.app.services.ICategoryOptionCrud;
+
+@Controller 
+@RequestMapping("/app/admin/questionsetup")
+public class QuestionSetUpController {
+
+	
+	@Value("${text.questionSetUpController.titleNewCategory}")
+	private String titleNewCategory;
+	
+	
+	
+	
+	@Value("${text.questionSetUpController.textAddNewCategory}")
+	private String textAddNewCategory;
+	
+	@Autowired
+	private ICategoryOptionCrud iCategoryOptionCrud;
+	
+	
+	
+	@GetMapping("/addnewcategory")
+	private String getFormNewCategoryOption(Model model) {
+		
+		CategoryOption categoryOption= new CategoryOption();
+		
+		
+		
+		model.addAttribute("titleNewCategory", titleNewCategory);
+		model.addAttribute("textAddNewCategory", textAddNewCategory);
+		model.addAttribute("categoryOption", categoryOption);	
+		
+		
+		return "addcategoryoption";
+	}
+	
+	
+	
+	@PostMapping("addnewcategory")
+	private String addNewCategoryOption(@Valid CategoryOption categoryOption, BindingResult result,  Model model) {
+		
+		if (result.hasErrors()) {
+			model.addAttribute("titleNewCategory", titleNewCategory);
+			model.addAttribute("textAddNewCategory", textAddNewCategory);
+			model.addAttribute("categoryOption", categoryOption);	
+			return "addcategoryoption";
+		}
+		
+		iCategoryOptionCrud.save(categoryOption);
+		
+		return "redirect:/app/index";
+	}
+	
+	
+	
+	
+	
+	@GetMapping ("/questions")
+	public String questionSetup() {
+		
+		
+		
+		return "questionsetup";
+	}
+	
+	
+}
--- src/main/java/com/pablop/survey/web/app/models/entity/CategoryOption.java
+++ src/main/java/com/pablop/survey/web/app/models/entity/CategoryOption.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name="category_options")
@@ -22,7 +23,7 @@ public class CategoryOption implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;	
 	
-	
+	@NotEmpty
 	@Column(name="category_name")
 	private String categoryName;
 	
